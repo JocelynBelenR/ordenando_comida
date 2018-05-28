c2389179744ba9ff395e69e2c93e0d18a57128ef
Jocelyn Rodriguez <jrodriguez9205@hotmail.com>
2018-05-27 21:59:17 -0400

    Primer intento del ejercicio

---
+++ app.js
@@ -0,0 +1,15 @@
+function orden_comida () {
+    let menu = ("ceviche","tacos_al_pastor","empanada_chilena","hamburguesa");
+      for (let i = 0; i < menu ; i++) {
+        console.log(menu);
+     }
+}
+orden_comida();
+
+/*2. ORDENANDO COMIDA
+Imagina que estás pidiendo comida en un restaurante. 
+Tu plato favorito es ceviche, así que lo elegirías si está en el menú. 
+Si no hay ceviche, elegirías tacos al pastor. 
+Si eso no está, te irías por empanada chilena. 
+En el caso raro que ninguna de esas opciones esté disponible, te irías por una hamburguesa. 
+Escribe una función, que de acuerdo a la disponibilidad de los platos en el menú, decidas qué ordenar.*/
